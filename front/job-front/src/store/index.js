5135008dd6925023813f719d7acb6a34bc703580
유환규 <hwanguebest@naver.com>
2023-03-06 00:08:23 +0900

    FEAT: 로그인 기능 추가

--- front/job-front/src/store/index.js
+++ front/job-front/src/store/index.js
@@ -5,12 +5,14 @@ import rootSaga from "./sagas";
 import LoginReducer from "./reducer/loginReducer";
 
 import createSagaMiddleware from "redux-saga";
+import JobReducer from "./reducer/jobReducer";
 
 const sagaMiddleware = createSagaMiddleware();
 
 // 객체의 형태를 정의하기 위해 ES6의 객체리터럴 단축문법을 사용
 const rootReducer = combineReducers({
   login: LoginReducer,
+  job: JobReducer,
 });
 
 export const store = configureStore({
--- front/job-front/src/store/sagas/index.js
+++ front/job-front/src/store/sagas/index.js
@@ -1,6 +1,6 @@
 import { call, all, put, takeEvery, takeLatest, fork } from "redux-saga/effects";
 import { doLogin, doJoin } from "store/sagas/user";
-
+import { doJob } from "store/sagas/job";
 export default function* rootSaga() {
-  yield all([fork(doLogin), fork(doJoin)]);
+  yield all([fork(doLogin), fork(doJoin), fork(doJob)]);
 }
